72085ed3d9297027894b1f832f3389a98ea5d0c8
Maxinary <max.scribs@gmail.com>
2016-04-02 23:13:08 -0400

    Added scalar unit

--- driver.js
+++ driver.js
@@ -1,30 +1,39 @@
 "use strict"
-var c = document.getElementById("game");
-c.width = document.body.clientWidth;
-c.height = document.body.clientHeight;
-var player = new boardObject(new Tuple(0,0), new Tuple(80,80), rect);
-player.color = "#FF0000";
-
-var box = new boardObject(new Tuple(-30,-30), new Tuple(500,500), rect);
-box.color = "#0000FF";
-
-var a = new gameBoard([box, player], 0, c);
-registerKeyPress(buttonMove.hold, 87, function(){player.point.y-=2;a.draw();});
-registerKeyPress(buttonMove.hold, 83, function(){player.point.y+=2;a.draw();});
-registerKeyPress(buttonMove.hold, 65, function(){player.point.x-=2;a.draw();});
-registerKeyPress(buttonMove.hold, 68, function(){player.point.x+=2;a.draw();});
-setInterval(
-  function(){
-    for(var i in holdFunctions){
-      if(holdFunctions[i]!==undefined){
-        if(keys[i] === true){
-          for(var j=0;j<holdFunctions[i].length;j++){
-            holdFunctions[i][j]();
+var unit;
+window.onload = function(){
+  var c = document.getElementById("game");
+  c.width = document.body.clientWidth;
+  c.height = document.body.clientHeight;
+  console.log(c.width,c.height);
+  if(c.width>c.height){
+    unit = c.height/256;
+  }else{
+    unit = c.width/256;
+  }
+  var player = new boardObject(new Tuple(0,0), new Tuple(64,64), rect);
+  player.color = "#FF0000";
+  
+  var box = new boardObject(new Tuple(0,0), new Tuple(128,128), rect);
+  box.color = "#0000FF";
+  
+  var a = new gameBoard([box, player], 0, c);
+  registerKeyPress(buttonMove.hold, 38, function(){player.point.y-=2;a.draw();});
+  registerKeyPress(buttonMove.hold, 40, function(){player.point.y+=2;a.draw();});
+  registerKeyPress(buttonMove.hold, 37, function(){player.point.x-=2;a.draw();});
+  registerKeyPress(buttonMove.hold, 39, function(){player.point.x+=2;a.draw();});
+  setInterval(
+    function(){
+      for(var i in holdFunctions){
+        if(holdFunctions[i]!==undefined){
+          if(keys[i] === true){
+            for(var j=0;j<holdFunctions[i].length;j++){
+              holdFunctions[i][j]();
+            }
           }
         }
       }
-    }
-    a.draw();
-  },
-  10
-);
+      a.draw();
+    },
+    10
+  );
+};
