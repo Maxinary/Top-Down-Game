d063cf77e16449d9edc654b1d003efbdb986387c
Maxinary <max.scribs@gmail.com>
2016-04-03 09:56:06 -0400

    Made rotate a general attribute

--- resource.js
+++ resource.js
@@ -20,13 +20,14 @@ class gameBoard{
     this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
     var cObj = this.objects[this.centerOn];
     for(var i=0;i < this.objects.length;i++){
-      this.objects[i].draw(
-        new Tuple(
-          -unit*(cObj.point.x+cObj.size.x/2) + this.canvas.width/2,
-          -unit*(cObj.point.y+cObj.size.y/2) + this.canvas.height/2
-        ),
-        this.context
-      );
+      this.context.save();
+      this.context.fillStyle = getColor(this.objects[i]);
+      this.context.translate(
+        -unit*(cObj.point.x+cObj.size.x/2) + this.canvas.width/2 + this.objects[i].point.x*unit + this.objects[i].size.x*unit/2, 
+        -unit*(cObj.point.y+cObj.size.y/2) + this.canvas.height/2+ this.objects[i].point.y*unit + this.objects[i].size.y*unit/2);
+      this.context.rotate(this.objects[i].angle);
+      this.objects[i].draw(this.context);
+      this.context.restore();
     }
   }
 }
@@ -36,5 +37,6 @@ class boardObject{
     this.point = point;
     this.size = size;
     this.draw = drawFunction;
+    this.angle = 0;
   }
 }
