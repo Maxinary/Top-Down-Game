c28506b0ba89875cafeef262491e48de2bfc6697
Maxinary <max.scribs@gmail.com>
2016-04-03 09:25:16 -0400

    Added rotation

--- customDrawFunctions.js
+++ customDrawFunctions.js
@@ -14,11 +14,16 @@ function dist(x1,y1,x2,y2){
 //drawing methods start here
 function rect(point, ctx){
   var t = ctx.fillStyle;
+  ctx.save();
   ctx.fillStyle = getColor(this);
-  ctx.fillRect(point.x + this.point.x*unit,
-               point.y + this.point.y*unit,
+  ctx.translate(point.x + this.point.x*unit + this.size.x*unit/2, 
+    point.y + this.point.y*unit + this.size.y*unit/2);
+  ctx.rotate(this.angle);
+  ctx.fillRect(-this.size.x*unit/2,
+               -this.size.y*unit/2,
                this.size.x*unit,
                this.size.y*unit
   );
+  ctx.restore();
   ctx.fillStyle = t;
 }
