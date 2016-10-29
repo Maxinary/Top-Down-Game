337a6fac3f1dc5f44d0226a560b4eb2ed94deb1b
Maximilian Victor Scribner <max.scribs@gmail.com>
2016-10-29 09:07:41 -0400

    Changed from setInterval to requestAnimationFrame

--- driver.js
+++ driver.js
@@ -10,17 +10,20 @@ function reloadRes(){
     unit = c.width/256;
   }
 }
+
+
+
 window.onload = function(){
   reloadRes();
   window.addEventListener("resize", reloadRes);
 
-  var player = new boardObject(new Tuple(0,0), new Tuple(32,32), rect);
+  var player = new boardObject(new Tuple(0,0), new Tuple(32,32), trig);
   player.color = "#FF0000";
   
   var box = new boardObject(new Tuple(0,0), new Tuple(128,128), rect);
   box.color = "#0000FF";
   
-  var a = new gameBoard([box, player], 0, c);
+  var a = new gameBoard([box, player], 1, c);
   var playerMove = function(a){
     player.point.y+=a*Math.sin(player.angle);
     player.point.x+=a*Math.cos(player.angle);  
@@ -29,19 +32,20 @@ window.onload = function(){
   registerKeyPress(buttonMove.hold, 40, function(){playerMove(-1);a.draw();});
   registerKeyPress(buttonMove.hold, 37, function(){player.angle -= 0.02;a.draw();});
   registerKeyPress(buttonMove.hold, 39, function(){player.angle += 0.02;a.draw();});
-  setInterval(
-    function(){
-      for(var i in holdFunctions){
-        if(holdFunctions[i]!==undefined){
-          if(keys[i] === true){
-            for(var j=0;j<holdFunctions[i].length;j++){
-              holdFunctions[i][j]();
-            }
+  
+  function totalLoop(){
+    for(var i in holdFunctions){
+      if(holdFunctions[i]!==undefined){
+        if(keys[i] === true){
+          for(var j=0;j<holdFunctions[i].length;j++){
+            holdFunctions[i][j]();
           }
         }
       }
-      a.draw();
-    },
-    10
-  );
+    }
+    a.draw();
+    requestAnimationFrame(totalLoop);
+  }
+  
+  requestAnimationFrame(totalLoop);
 };
